26b7546cc6b9e5db6fd1fc25124274b41ca9480b
dgnvr <dgnvrrg@gmail.com>
2021-02-24 12:15:29 -0600

    Creacion de tablas perfiles, usuarios y categorias

---
+++ database/migrations/2021_02_24_170330_profiles_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->float('money');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->double('money');
+            $table->string('type');
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('rel-profile-category', function (Blueprint $table){
+            $table->id();
+            $table->unsignedBigInteger('profile_id');
+            $table->foreign('profile_id')->references('id')->on('profiles');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('rel-profile-category');
+    }
+}
