a715a9001e6b4bc1d0c3f5211681c2ac21e3ce5e
AlanMendoza09 <37988489+AlanMendoza09@users.noreply.github.com>
2021-02-25 14:41:41 -0600

    Modificacion a index profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>List of profiles</h1>
-<p>
-    Create a profile, <a href="{{route('profiles.create')}}">click here.</a>
-</p>
-<table>
+<div class="container">
+<h2>List of profiles</h2>
+    <form action="{{ route('profiles.create') }}" >
+        <input type="submit" class="btn btn-success" value="Create profile">
+    </form>
+    <div>
+<table class="table table-hover">
     <thead>
         <tr>
             <th>#</th>
@@ -23,17 +25,22 @@
                 <td>{{ $item->money }}</td>
                 <td>{{ $item->type }}</td>
                 <td>{{ $item->usuario_id }}</td>
-                <td><a href="{{route('profiles.edit', $item->id ) }}"> Edit</a></td>
+                <td>
+                    <form action="{{route('profiles.edit', $item->id ) }}">
+                        <input type="submit" class="btn btn-primary" value="Edit">
+                    </form>
+                </td>
                 <td>
                     <form action="{{ route('profiles.destroy', $item->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button type="submit">Remove</button>
+                        <button type="submit" class="btn btn-danger">Remove</button>
                     </form>
                 </td>
             </tr>
         @endforeach
     </tbody>
 </table>
+</div>
 
 @endsection
