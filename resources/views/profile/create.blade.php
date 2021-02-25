9b3891e4815a6d6bd7d822dd2bba1e7d76094cc7
AlanMendoza09 <37988489+AlanMendoza09@users.noreply.github.com>
2021-02-25 14:03:10 -0600

    Diseño a blades

--- resources/views/profile/create.blade.php
+++ resources/views/profile/create.blade.php
@@ -1,14 +1,27 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>Profile creator!</h1>
+<div class="container">
+<h2>Profile creator!</h2>
 <form action="{{ route('profiles.store') }}" method="POST">
     @csrf
-    Name:<input type="text" name="name"> <br>
-    Money:<input type="number" name="money"> <br>
-    Type:<input type="text" name="type"> <br>
-    User_id:<input type="text" name="usuario_id"> <br>
-    <button type="submit">Create profile</button>
+    <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" class="form-control" name="name">
+    </div>
+    <div class="form-group">
+        <label for="money">Money: $</label>
+        <input type="number" class="form-control" name="money">
+    </div>
+    <div class="form-group">
+        <label for="type">Type</label>
+        <input type="text" class="form-control" name="type">
+    </div>
+    <div class="form-group">
+        <label for="usuario_id">User Id:</label>
+        <input type="text" class="form-control" name="usuario_id">
+    </div>
+    <button type="submit" class="btn btn-primary">Create profile</button>
 </form>
-
+</div>
 @endsection
